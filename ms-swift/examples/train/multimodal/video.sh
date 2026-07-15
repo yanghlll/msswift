09eddf45ec342051b64837c0a2ad11a9a2242582
yifan <1638977449@qq.com>
2026-07-15 23:11:52 +0400

    change the streaming video training script

--- ms-swift/examples/train/multimodal/video.sh
+++ ms-swift/examples/train/multimodal/video.sh
@@ -11,9 +11,9 @@ swift sft \
     --model /data1/zlx/cache/huggingface/hub/model/llavaov2 \
     --model_type llava_onevision2 \
     --dataset /data1/zlx/cache/huggingface/hub/video_data/test/chat_TimeLens-100K_000_1fps_abs.jsonl \
+    --new_special_tokens '</silence>' '</response>' \
     --load_from_cache_file True \
-    --split_dataset_ratio 0.01 \
-    --tuner_type lora \
+    --tuner_type full \
     --torch_dtype bfloat16 \
     --num_train_epochs 10 \
     --per_device_train_batch_size 1 \
@@ -22,8 +22,8 @@ swift sft \
     --lora_rank 8 \
     --lora_alpha 32 \
     --target_modules all-linear \
-    --freeze_vit true \
-    --freeze_aligner true \
+    --freeze_vit false \
+    --freeze_aligner false \
     --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
     --eval_steps 50 \
     --save_steps 50 \
@@ -35,5 +35,7 @@ swift sft \
     --dataloader_num_workers 4 \
     --deepspeed zero2 \
     --dataset_num_proc 4
+
+#    --split_dataset_ratio 0.01 \
 
 
